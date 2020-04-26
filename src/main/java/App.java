361ca3e4627f1fdfc034372d6e498b94edbe134e
paulo-789 <jorhnepaulone@gmail.com>
2020-04-27 01:23:53 +0300

    added functionality to add a hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Add;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -13,5 +14,13 @@ public class App {
             Map<String,Object> model = new HashMap<String,Object>();
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
+
+        post("/add/new",(request, response) -> {
+            Map<String,Object>model = new HashMap<String, Object>();
+            String content = request .queryParams("content");
+            Add newAdd = new Add(content);
+            return new ModelAndView(model,"success.hbs");
+        },new HandlebarsTemplateEngine());
     }
+
 }
